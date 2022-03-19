d04a003aab847b4dbfd8067566ff7b39f36e4373
Nhatquangt45 <trannhatquang5402@gmail.com>
2022-03-19 10:39:14 +0700

    bai code cua quang

---
+++ main.js
@@ -0,0 +1,236 @@
+/**
+ * bài 1:
+ *
+ *
+ *
+ * --đầu vào :
+ *
+ *
+ * --xử lý:
+ *
+ *
+ * --đầu ra:
+ *
+ *
+ */
+
+document.getElementById("btnXuat").onclick = function () {
+  var ngay = document.getElementById("txtNgay").value * 1;
+  var thang = document.getElementById("txtThang").value * 1;
+  var nam = document.getElementById("txtNam").value * 1;
+
+  var x;
+  switch (ngay) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      x = 31;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      x = 30;
+      break;
+    case 2:
+      if (nam % 400 == 0 || (nam % 4 == 0 && nam % 100 != 0)) x = 29;
+      else x = 28;
+    default:
+      x = 0;
+      break;
+  }
+
+  if (ngay == x) {
+    if (thang == 12) {
+      ngay = 1;
+      thang = 1;
+      nam++;
+    } else {
+      ngay = 1;
+      thang++;
+    }
+  } else {
+    ngay++;
+  }
+
+  var kqTim = "ngày :" + ngay + " tháng: " + thang + " Năm: " + nam;
+  document.getElementById("tbXuat").innerHTML = kqTim;
+};
+
+/**bài 2
+ *
+ */
+
+document.getElementById("btnTimb2").onclick = function () {
+  var thang = document.getElementById("txtThangb2").value * 1;
+  var nam = document.getElementById("txtNamb2").value * 1;
+  var y;
+  switch (thang) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      y = 31;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      y = 30;
+      break;
+    case 2:
+      if (nam % 4 == 0) {
+        y = 29;
+      } else {
+        y = 28;
+      }
+      break;
+  }
+  kqTimb2 = "Tháng " + thang + " Có " + y + " Ngày !";
+  document.getElementById("txtTimb2").innerHTML = kqTimb2;
+};
+// Bài 3
+
+document.getElementById("btnIn").onclick = function () {
+  var soNguyen = document.getElementById("soNguyen").value;
+  var tram = Math.floor(soNguyen / 100);
+  var chuc = Math.floor((soNguyen % 100) / 10);
+  var dv = soNguyen % 10;
+  switch (tram) {
+    case 1:
+      tram = "Mot Tram ";
+      break;
+    case 2:
+      tram = "Hai Tram ";
+      break;
+    case 3:
+      tram = "Ba Tram ";
+      break;
+    case 4:
+      tram = "Bon Tram ";
+      break;
+    case 5:
+      tram = "Nam Tram ";
+      break;
+    case 6:
+      tram = "Sau Tram ";
+      break;
+    case 7:
+      tram = "Bay Tram ";
+      break;
+    case 8:
+      tram = "Tam Tram ";
+      break;
+    case 9:
+      tram = "Chin Tram ";
+      break;
+  }
+  switch (chuc) {
+    case 0:
+      if (chuc == 0) {
+        chuc = " ";
+        break;
+      } else {
+        chuc = "Le ";
+        break;
+      }
+    case 1:
+      chuc = "Muoi ";
+      break;
+    case 2:
+      chuc = "Hai Muoi ";
+      break;
+    case 3:
+      chuc = "Ba Muoi ";
+      break;
+    case 4:
+      chuc = "Bon Muoi ";
+      break;
+    case 5:
+      chuc = "Nam Muoi ";
+      break;
+    case 6:
+      chuc = "Sau Muoi ";
+      break;
+    case 7:
+      chuc = "Bay Muoi ";
+      break;
+    case 8:
+      chuc = "Tam Muoi ";
+      break;
+    case 9:
+      chuc = "Chin Muoi ";
+      break;
+  }
+  switch (dv) {
+    case 0:
+      if (dv == 0) {
+        dv = " ";
+        break;
+      } else {
+        dv = "Le ";
+        break;
+      }
+    case 5:
+      if (dv == 0) {
+        dv = "Lam ";
+        break;
+      } else {
+        dv = "Nam ";
+        break;
+      }
+    case 1:
+      dv = "Mot";
+      break;
+    case 2:
+      dv = "Hai";
+      break;
+    case 3:
+      dv = "Ba";
+      break;
+    case 4:
+      dv = "Bon";
+      break;
+    case 6:
+      dv = "Sau";
+      break;
+    case 7:
+      dv = "Bay";
+    case 8:
+      dv = "Tam";
+      break;
+    case 9:
+      dv = "Chin";
+      break;
+  }
+  kqIn = "Cách Đọc Số Là : " + tram + chuc + dv;
+  document.getElementById("txtIn").innerHTML = kqIn;
+};
+// Bài 4
+document.getElementById("btnInSV").onclick = function () {
+  var SV1 = document.getElementById("txtSV1").value * 1;
+  var SV2 = document.getElementById("txtSV2").value * 1;
+  var SV3 = document.getElementById("txtSV3").value * 1;
+  var truong = document.getElementById("txtTruong").value * 1;
+  var xaNhat;
+  var toaDo1 = Math.abs(truong - SV1);
+  var toaDo2 = Math.abs(truong - SV2);
+  var toaDo3 = Math.abs(truong - SV3);
+  if (toaDo1 > toaDo2 && toaDo1 > toaDo3) {
+    xaNhat = "SV1 Xa Trường Đại Học Nhất";
+  } else if (toaDo2 > toaDo1 && toaDo2 > toaDo3) {
+    xaNhat = "SV2 Xa Trường Đại Học Nhất";
+  } else if (toaDo3 > toaDo1 && toaDo3 > toaDo2) {
+    xaNhat = "SV3 Xa Trường Đại Học Nhất";
+  } else {
+    ("Sai ở Đâu đó !!!");
+  }
+  document.getElementById("txtInSV").innerHTML = xaNhat;
+};
